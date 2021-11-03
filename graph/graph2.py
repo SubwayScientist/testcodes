41ec9c27c53b9665fecddc541c4e9eb0c9482cde
Aubert <aubertlamy@gmail.com>
2021-11-03 17:47:17 -0400

    Done with graph2 and graph3 for labo2 - exp3

--- graph/graph2.py
+++ graph/graph2.py
@@ -2,17 +2,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from CSV import read_three_col_csv
 
+# Gives our set of data in three lists.
 T, fi, dfi = read_three_col_csv('data_lab2.csv')
 
+# Convert these lists in arrays.
 T = np.array(T)                                
 fi = np.array(fi)
 dfi = np.array(dfi)                                
 
+# Gives the coeff. of a deg 1 function fitting for our set of data.
 b, covariance = np.polyfit(T, fi, deg=1, cov=True)
 
+# Return de standard deviation of elements of b.
 stddev_b0 = np.sqrt(covariance[0,0])
 stddev_b1 = np.sqrt(covariance[1,1])
 
+# Max and min function of our linear relation.
 b_upper = b + np.array([stddev_b0, -stddev_b1])
 b_lower = b + np.array([-stddev_b0, stddev_b1])
 
@@ -32,12 +37,15 @@ plt.ylabel(r"% isooctane")
 plt.xlim(27, 39)
 plt.ylim(62, 70)
 
+# Plot a cluster of our data set.
 plt.errorbar(T, fi, xerr=0.02, yerr=dfi, fmt='o')
 
+# Plot the linear relation and its error over the same figure.
 plt.plot(x, y, color='black')
 plt.plot(x, y_upper, color='r', linestyle='--')
 plt.plot(x, y_lower, color='r', linestyle='--')
 plt.fill_between(x, y_upper, y_lower, color='r', alpha=0.3)
+
 plt.savefig('graph2.png')
 
 plt.show()
--- graph/graph3.py
+++ graph/graph3.py
@@ -4,23 +4,41 @@ import matplotlib.pyplot as plt
 
 
 def function(x, Tc, B):
-    """ Photoluminescence with two diffusion channels """
+    """ Relation for isoctane-methanol solution """
     beta = 0.327
     T = np.array([29.0, 32.07, 36.1, 38.07, ])
     x = np.log(Tc - T)
     return -beta*x + np.log(Tc**beta / B)
 
+# Array for our Temperatures and slope related to them
 T = np.array([29.0, 32.07, 36.1, 38.07, ])
 a = np.array([0.0208, 0.0231, 0.0258, 0.0277])
+
+# Entry parameter before optimizing.
 Tc = 50
+
 x = np.log(Tc - T)
 y = np.log(a)
 
+# Function that finds the optimal parameters for our relation.
 popt, pcov = scipy.optimize.curve_fit(
 function, x, y, p0=[50., 1.0])
 
+# Error on our values.
+stddev_Tc = np.sqrt(pcov[0,0])
+stddev_B = np.sqrt(pcov[1,1])
+
 print(popt)
+print(stddev_Tc)
+
+
+X = np.log(popt[0]-T)
 
 plt.figure()
-plt.errorbar(x, function(x, popt[0], popt[1]), xerr=0., yerr=0., fmt='o')
+plt.xlabel(r"log(Tc - T)")
+plt.ylabel(r"log(pente)")
+
+plt.errorbar(X, y, xerr=0., yerr=0., fmt='o')
+plt.plot(X, function(X, popt[0], popt[1]))
 plt.show()
+
